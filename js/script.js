25ca0ffa69598265d40330da993217583c2fbde5
Eleazer-Afotey <eleazerallnice@gmail.com>
2022-01-06 20:39:42 -0500

    added portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+/*
+TYPEWRITER EFFECT
+used in banner
+*/
+
 var app = document.querySelector(".typewriter");
 
 app.style.color = "#0275d8";
@@ -16,6 +21,11 @@ typewriter
   .pauseFor(2000)
   .start();
 
+/*
+  TABLIST FUNCTION
+  toggle 
+  */
+
 let tablist = document.querySelectorAll(".nav-tabs .nav-item a");
 
 tablist.forEach(function (e) {
@@ -23,5 +33,44 @@ tablist.forEach(function (e) {
   e.addEventListener("click", function (e) {
     e.preventDefault();
     trigger.show();
+  });
+});
+
+/*
+PORTFOLIO IMAGES EFFECT
+*/
+
+var grid = document.querySelector(".grid");
+let iso;
+
+imagesLoaded(grid, function () {
+  // Isotope js function
+  iso = new Isotope(grid, {
+    itemSelector: ".grid-item",
+    layoutMode: "masonry",
+  });
+
+  var filterEl = document.querySelector(".portfolio-filter");
+  filterEl.addEventListener("click", function (event) {
+    if (!matchesSelector(event.target, "button")) {
+      return;
+    }
+    var filterValue = event.target.getAttribute("data-filter");
+    iso.arrange({
+      filter: filterValue,
+    });
+  });
+});
+
+var btnElement = document.querySelectorAll(".fil-btn");
+btnElement.forEach(function (el, key) {
+  el.addEventListener("click", function () {
+    el.classList.add("clicked");
+
+    btnElement.forEach(function (ell, keys) {
+      if (key != keys) {
+        ell.classList.remove("clicked");
+      }
+    });
   });
 });
